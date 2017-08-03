09ecde5a996d35a74b45fd6aae76cc0ec0b63cbc
xinyi <xinyi.kao@titansoft.com.sg>
2017-08-03 23:59:27 +0800

    add project

---
+++ StringAverage/Kata.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAverage
+{
+    public class Kata
+    {
+        public string[] NumberString = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        public string AverageString(string input)
+        {
+            if (input == "")
+            {
+                return "n/a";
+            }
+            var numbers = input.Split(' ');
+            var total = numbers.Sum(number => ConvertToInt(number));
+
+            var average = total / numbers.Length;
+
+            return NumberString[average];
+        }
+
+        private int ConvertToInt(string input)
+        {
+            switch (input)
+            {
+                case "zero":
+                    return 0;
+                    break;
+                case "one":
+                    return 1;
+                    break;
+                case "two":
+                    return 2;
+                    break;
+                case "three":
+                    return 3;
+                    break;
+                case "four":
+                    return 4;
+                    break;
+                case "five":
+                    return 5;
+                    break;
+                case "six":
+                    return 6;
+                    break;
+                case "seven":
+                    return 7;
+                    break;
+                case "eight":
+                    return 8;
+                    break;
+                case "nine":
+                    return 9;
+                    break;
+                default:
+                    return 0;
+                    break;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ StringAverage/KataUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace StringAverage
+{
+    [TestFixture]
+    public class KataUnitTest
+    {
+        [TestCase("four", "zero nine five two")]
+        [TestCase("three", "four six two three")]
+        [TestCase("three", "one two three four five")]
+        [TestCase("four", "five four")]
+        [TestCase("zero", "zero zero zero zero zero")]
+        [TestCase("two", "one one eight one")]
+        [TestCase("n/a", "")]
+        public void StringNumberAverageTest(string expected, string inputString)
+        {
+            var kata = new Kata();
+            var actual = kata.AverageString(inputString);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
