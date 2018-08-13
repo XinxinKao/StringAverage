edaba667f8cc332a264576547518ade9f03ece73
xinyi <xinyi.kao@titansoft.com.sg>
2018-08-13 19:13:28 +0800

    add test project

---
+++ StringAverageTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringAverageTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringAverageTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("92cb3714-2902-4966-8573-7815b90e5b96")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringAverageTest/UnitTest1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringAverage;
+
+namespace StringAverageTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod()]
+        public void StringNumberAverageTest_1()
+        {
+            var inputString = "zero nine five two";
+            var expected = "four";
+            var kata = new Kata();
+            var actual = kata.AverageString(inputString);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void StringNumberAverageTest_2()
+        {
+            var inputString = "four six two three";
+            var expected = "three";
+            var kata = new Kata();
+            var actual = kata.AverageString(inputString);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void StringNumberAverageTest_3()
+        {
+            var inputString = "one two three four five";
+            var expected = "three";
+            var kata = new Kata();
+            var actual = kata.AverageString(inputString);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void StringNumberAverageTest_4()
+        {
+            var inputString = "five four";
+            var expected = "four";
+            var kata = new Kata();
+            var actual = kata.AverageString(inputString);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void StringNumberAverageTest_5()
+        {
+            var inputString = "zero zero zero zero zero";
+            var expected = "zero";
+            var kata = new Kata();
+            var actual = kata.AverageString(inputString);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void StringNumberAverageTest_6()
+        {
+            var inputString = "one one eight one";
+            var expected = "two";
+            var kata = new Kata();
+            var actual = kata.AverageString(inputString);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void StringNumberAverageTest_7()
+        {
+            var inputString = "";
+            var expected = "n/a";
+            var kata = new Kata();
+            var actual = kata.AverageString(inputString);
+            Assert.AreEqual(expected, actual);
+        }
+	}
+}
